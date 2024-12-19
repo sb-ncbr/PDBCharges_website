fe699f0bbc76df7dcdd63abf44232a73e6fae155
dargen3 <dargen3@centrum.cz>
2024-12-19 09:35:13 +0100

    fix

--- app/routes.py
+++ app/routes.py
@@ -14,22 +14,29 @@ application.jinja_env.lstrip_blocks = True
 application.config['SECRET_KEY'] = str(random())
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
+def log_access(request, code):
+    with open(f'{root_dir}/calculated_structures/accesses.txt', 'a') as log_file:
+        log_file.write(f'{request.remote_addr} {code} {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n')
+
+def download_data(code):
+    data_dir = f'{root_dir}/calculated_structures/{code}'
+    os.system(f"mkdir {data_dir}")
+    s3_url = "https://s3.cl4.du.cesnet.cz/46b646c0_b0c7_45dd_8c7f_29536a545ca7:ceitec-biodata-pdbcharges"
+    for file in [f"{code}.cif", f"output.txt", f"residual_warnings.json"]:
+        response = requests.get(f'{s3_url}/{code}/{file}')
+        if response.status_code == 200:
+            with open(f'{data_dir}/{file}', 'w') as pdb_file:
+                pdb_file.write(response.text)
+
 @application.route('/', methods=['GET', 'POST'])
 def main_site():
     if request.method == 'POST':
         code = request.form['code'].strip().lower()
-        with open(f'{root_dir}/calculated_structures/accesses.txt', 'a') as log_file:
-            log_file.write(f'{request.remote_addr} {code} {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n')
         data_dir = f'{root_dir}/calculated_structures/{code}'
         if not os.path.isdir(data_dir):
-            os.system(f"mkdir {data_dir}")
-            s3_url = "https://s3.cl4.du.cesnet.cz/46b646c0_b0c7_45dd_8c7f_29536a545ca7:ceitec-biodata-pdbcharges"
-            for file in [f"{code}.cif", f"output.txt", f"residual_warnings.json"]:
-                response = requests.get(f'{s3_url}/{code}/{file}')
-                if response.status_code == 200:
-                    with open(f'{data_dir}/{file}', 'w') as pdb_file:
-                        pdb_file.write(response.text)
+            download_data(code)
         if not os.path.exists(f'{root_dir}/calculated_structures/{code}/{code}.cif'):
+            log_access(request, code)
             message = Markup(
                 f'There is no results for structure with PDB ID <strong>{code}</strong>. The possible causes are:'
                 f'<ul> '
@@ -46,7 +53,22 @@ def main_site():
 @application.route('/results')
 def results():
     code = request.args.get('code').lower()
+    log_access(request, code)
     data_dir = f'{root_dir}/calculated_structures/{code}'
+    if not os.path.isdir(data_dir):
+        download_data(code)
+    if not os.path.exists(f'{root_dir}/calculated_structures/{code}/{code}.cif'):
+
+        log_access(request, code)
+        message = Markup(
+            f'There is no results for structure with PDB ID <strong>{code}</strong>. The possible causes are:'
+            f'<ul> '
+            f'<li>A structure with such a PDB ID does not exist.</li>'
+            f'<li>The structure with hydrogens has more than 99999 atoms.</li>'
+            f'<li>The structure contains serious errors and cannot be used as input for calculating the partial atomic charge.</li></ul>  ')
+        flash(message, 'warning')
+        return render_template('results.html', code="None", n_ats="None", total_charge="None",
+                               num_of_non_charge_atoms="None")
 
     with open(f"{data_dir}/{code}.cif", "r") as cif_file:
         charges = []
@@ -68,10 +90,14 @@ def results():
 def download_files():
     code = request.args.get('code')
     data_dir = f'{root_dir}/calculated_structures/{code}'
-    with zipfile.ZipFile(f'{data_dir}/{code}_charges.zip', 'w') as zip:
-        zip.write(f'{data_dir}/{code}.cif', arcname=f'{code}.cif')
-        zip.write(f'{data_dir}/residual_warnings.json', arcname=f'residual_warnings.json')
-    return send_from_directory(data_dir, f'{code}_charges.zip', as_attachment=True)
+    try:
+        with zipfile.ZipFile(f'{data_dir}/{code}_charges.zip', 'w') as zip:
+            zip.write(f'{data_dir}/{code}.cif', arcname=f'{code}.cif')
+            zip.write(f'{data_dir}/residual_warnings.json', arcname=f'residual_warnings.json')
+        return send_from_directory(data_dir, f'{code}_charges.zip', as_attachment=True)
+    except FileNotFoundError:
+        return render_template('404.html'), 404
+
 
 @application.route('/structure/<code>')
 def get_structure(code: str):
