be41c130e26a35edaf4ad89e060a506d39d0404f
Dominik Tichy <tichydominik451@gmail.com>
2025-02-01 20:03:54 +0100

    added smoothening functoin

---
+++ viewer/src/charges-extension/utils.ts
@@ -0,0 +1,23 @@
+export function erf(x: number) {
+  const a1 = 0.254829592;
+  const a2 = -0.284496736;
+  const a3 = 1.421413741;
+  const a4 = -1.453152027;
+  const a5 = 1.061405429;
+  const p = 0.3275911;
+
+  const sign = x >= 0 ? 1 : -1;
+  x = Math.abs(x);
+
+  const t = 1.0 / (1.0 + p * x);
+  const y =
+    1.0 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-x * x);
+
+  return sign * y;
+}
+
+// np.round(x) + erf((x - np.round(x))*5) * 0.5
+export function smoothCharge(x: number) {
+  const rounded = Math.round(x);
+  return rounded + erf((x - rounded) * 5) * 0.5;
+}
